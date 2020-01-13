2470eca4d994c8000a0e08dd3656f1b317b79479
Indrek Juhkam <indrek@urgas.eu>
2020-01-13 15:42:02 +0200

    Add ephemeral port monitor
    * collect.sh has all the collection logic
    * Metrics are served with nginx (/metrics)
    
    EN-5294

---
+++ collect.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Collects information about ephemeral port usage.
+#
+# The container needs ss and awk:
+#   apk add iproute2
+
+set -euo pipefail
+
+# Sets minimal number of ports that have to be used for {local_ip + peer} group
+# before showing it in the metrics list.
+MIN_COUNT_TO_TRACK="${MIN_COUNT_TO_TRACK:-10}"
+
+min_local_port=$(cut -f 1 /proc/sys/net/ipv4/ip_local_port_range)
+max_local_port=$(cut -f 2 /proc/sys/net/ipv4/ip_local_port_range)
+
+# Connections are in this format:
+# State   Recv-Q   Send-Q   Local Address:Port   Peer Address::Port
+# ESTAB   0        0        10.4.134.246:54060   172.20.0.1:443
+fetch_connections() {
+  ss --numeric \
+     --no-header \
+     --ipv4 \
+     --tcp \
+     state connected "( sport >= :$min_local_port and sport <= :$max_local_port )"
+}
+
+format_connection_to_local_ip_with_peer() {
+  # Formats connection as "local_ip-peer_ip:peer_port"
+  awk -F":|[ \t]+" '{print $4 "-" $6 ":" $7}'
+}
+
+print_metrics() {
+  # Takes in lines of local ip with peer. We use uniques var to count each
+  # occurrence and then print it out in OpenMetrics format
+  #
+  # This is similar to `sort -u | uniq -c | awk '...'` but using awk for
+  # counting is a lot faster.
+  awk '
+    { uniques[$1]++ }
+    END {
+      for(key in uniques)
+        if (uniques[key] >= ENVIRON["MIN_COUNT_TO_TRACK"])
+          printf("system.net.used_ports.count{local_ip_with_peer=\"%s\"} %s\n", key, uniques[key])
+    }
+  '
+}
+
+echo "# TYPE system.net.used_ports.max_count gauge"
+echo "system.net.used_ports.max_count $((max_local_port-min_local_port))"
+
+echo "# TYPE system.net.used_ports.count gauge"
+fetch_connections | \
+  format_connection_to_local_ip_with_peer | \
+  print_metrics
